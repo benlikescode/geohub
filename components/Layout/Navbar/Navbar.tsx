23e3904e1cb77371b332c19582a1b5ecbcc576cb
Ben <benhoeg@outlook.com>
2021-10-08 23:40:07 -0300

    Added signup/login btns to nav if not signed in

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react'
 import { StyledNavbar } from '.'
 import { BellIcon } from '@heroicons/react/outline'
-import { Avatar, Button, Icon, Searchbar } from '../../System'
+import { Avatar, Button, FlexGroup, Icon, Searchbar } from '../../System'
 import { selectUser } from '../../../redux/user'
 import { useSelector } from 'react-redux'
 import { UserType } from '../../../types'
+import Link from 'next/link'
 
 const Navbar: FC = () => {
   const user: UserType = useSelector(selectUser)
@@ -13,18 +14,34 @@ const Navbar: FC = () => {
     <StyledNavbar>
       <h2 className="title">GeoHub</h2>
 
-      <div className="rightWrapper">
-        <Searchbar />
-        <Button type="icon">
-          <Icon size={24}>
-            <BellIcon />
-          </Icon>
-        </Button>  
-        <div className="userInfo">
-          <Avatar url={user.avatar} alt="" size={30}/>
-          <span className="username">{user.name}</span>
+      {user.name ? 
+        <div className="rightWrapper">
+          <Searchbar />
+          <Button type="icon">
+            <Icon size={24}>
+              <BellIcon />
+            </Icon>
+          </Button>  
+          <div className="userInfo">
+            <Avatar url={user.avatar} alt="" size={30}/>
+            <span className="username">{user.name}</span>
+          </div>
         </div>
-      </div>
+        :
+        <FlexGroup gap={15}>
+          <Link href="/login">
+           <a>
+            <Button type="ghost" width="120px">Login</Button>
+           </a>
+          </Link>
+
+          <Link href="/register">
+            <a>
+              <Button type="solidBlue" width="120px">Sign Up</Button>
+            </a>
+          </Link>       
+        </FlexGroup>       
+    }    
     </StyledNavbar>
   )
 }
