83bf8b1af0b3366449b8fc0c0563d0ad09559f58
Ben <benhoeg@outlook.com>
2021-11-07 13:07:58 -0400

    Removed firebase from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Provider, Provider as ReduxProvider } from 'react-redux'
+import { Provider } from 'react-redux'
 import { store, persistor } from '../redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
-import { config } from '../utils/firebaseConfig'
-import { initializeApp } from 'firebase/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  const firebaseApp = initializeApp(config)
-
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
