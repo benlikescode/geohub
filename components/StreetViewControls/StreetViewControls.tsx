9d520736523f743d697f205287e2cd152de7ac8c
Ben <benhoeg@outlook.com>
2021-11-09 18:54:07 -0400

    Updated Game Controls

--- components/StreetViewControls/StreetViewControls.tsx
+++ components/StreetViewControls/StreetViewControls.tsx
@@ -1,38 +1,19 @@
 import { FlagIcon, LocationMarkerIcon } from '@heroicons/react/outline'
 import React, { FC, useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { StyledStreetViewControls } from '.'
-import { returnToStart } from '../../redux/game'
 import { Button, Icon, Tooltip } from '../System'
 
 const StreetViewControls: FC = () => {
-  const [showTip1, setShowTip1] = useState(false)
-  const [showTip2, setShowTip2] = useState(false)
-  const dispatch = useDispatch()
+  const [showTip, setShowTip] = useState(false)
 
   const handleBackToStart = () => {
-    dispatch(returnToStart({
-      atStart: true
-    }))
     // temp solution lol
     window.location.reload()
   }
 
-  const handleSetCheckpoint = () => {
-
-  }
-
   return (
     <StyledStreetViewControls>
-      <div className="controlBtn" onMouseEnter={() => setShowTip1(true)} onMouseLeave={() => setShowTip1(false)}>
-        <Button type="iconRounded" callback={handleSetCheckpoint}>
-          <Icon size={24} fill="var(--color2)">
-            <LocationMarkerIcon />
-          </Icon>
-        </Button>
-      </div>
-
-      <div className="controlBtn" onMouseEnter={() => setShowTip2(true)} onMouseLeave={() => setShowTip2(false)}>
+      <div className="controlBtn" onMouseEnter={() => setShowTip(true)} onMouseLeave={() => setShowTip(false)}>
         <Button type="iconRounded" callback={handleBackToStart}>
           <Icon size={24} fill="var(--color2)">
             <FlagIcon />
@@ -40,10 +21,8 @@ const StreetViewControls: FC = () => {
         </Button>
       </div>
 
-      {showTip1 && <Tooltip label="Create Checkpoint" top={90} left={70} />}
-      {showTip2 && <Tooltip label="Back To Start" top={145} left={70} />}
-    </StyledStreetViewControls>
-    
+      {showTip && <Tooltip label="Back To Start" top={8} left={60} />}
+    </StyledStreetViewControls>  
   )
 }
 
