c0a35030f2a5d3a70e574dbf784c2f574c11008f
Ben <benhoeg@outlook.com>
2021-11-09 03:33:03 -0400

    Removed unused component

--- components/Results/LeaderboardCard/LeaderboardItem/LeaderboardItem.Styled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const StyledLeaderboardItem = styled.div`
-  
-`
-
-export default StyledLeaderboardItem
--- components/Results/LeaderboardCard/LeaderboardItem/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as LeaderboardItem } from './LeaderboardItem'
-export { default as StyledLeaderboardItem } from './LeaderboardItem.Styled'
