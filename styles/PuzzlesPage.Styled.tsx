a40d2e1780e4e2489c25605aae8d03890416d21f
Ben <benhoeg@outlook.com>
2021-11-14 02:02:29 -0400

    Added Puzzles

---
+++ styles/PuzzlesPage.Styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledPuzzlesPage = styled.div`
+  #map {
+    height: calc(100vh - 180px);
+    width: 100%;
+    position: relative;
+  }
+`
+
+export default StyledPuzzlesPage
+
