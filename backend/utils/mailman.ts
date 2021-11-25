bf642b98f8c530be982a954d927353a163d8b4be
Ben <benhoeg@outlook.com>
2021-11-24 22:09:59 -0400

    test api route

--- backend/utils/mailman.ts
+++ backend/utils/mailman.ts
@@ -15,7 +15,7 @@ export const mailman = async (
   }
 
   try {
-    const res = await fetch(`https://geohub-prod-benlikescode.vercel.app/api/${endpoint}`, fetchConfig)
+    const res = await fetch(`/api/${endpoint}`, fetchConfig)
     return { status: res.status, res: await res.json() }
   }
   catch (err) {
