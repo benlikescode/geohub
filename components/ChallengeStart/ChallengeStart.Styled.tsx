c479877679ce5ab56f28358ef4ead0512cb5c381
Ben <benhoeg@outlook.com>
2022-06-05 19:59:21 -0300

    Updated Challenge Start UI

--- components/ChallengeStart/ChallengeStart.Styled.tsx
+++ components/ChallengeStart/ChallengeStart.Styled.tsx
@@ -1,11 +1,70 @@
 import styled from 'styled-components'
 
-type StyledProps = {
-  
-}
+type StyledProps = {}
 
 const StyledChallengeStart = styled.div<StyledProps>`
-  
+  .challengeStartWrapper {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    height: fit-content;
+    gap: 1.5rem;
+    padding: 4rem 1rem;
+    border-bottom: 1px solid #222;
+    width: 100%;
+    background-color: #121212;
+  }
+
+  .challengeTitle {
+    font-size: 3rem;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .challengeCreator {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+
+    span {
+      margin-left: 0.5rem;
+    }
+  }
+
+  .challengeBtn {
+    padding: 1rem 3rem;
+    border-radius: 50rem;
+    background-color: var(--mediumPurple);
+    color: #fff;
+    border: 1px solid rgba(255, 255, 255, 0.15);
+    font-size: 1rem;
+    font-weight: 500;
+
+    :hover {
+      background-color: var(--darkPurple);
+      transform: scale(1.02);
+    }
+  }
+
+  .challengeSettings {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    padding: 3rem;
+    flex-wrap: wrap;
+  }
+
+  .settingsItem {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    width: 220px;
+    border: 1px solid #202020;
+    border-radius: 6px;
+    padding: 1rem 0;
+  }
 `
 
 export default StyledChallengeStart
--- components/ChallengeStart/ChallengeStart.tsx
+++ components/ChallengeStart/ChallengeStart.tsx
@@ -1,10 +1,17 @@
+import {
+  ArrowsExpandIcon,
+  ClockIcon,
+  SwitchHorizontalIcon,
+  ZoomInIcon,
+} from '@heroicons/react/outline'
 import { useRouter } from 'next/router'
 import React, { FC, useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { StyledChallengeStart } from '.'
 import { selectUser } from '../../redux/user'
 import { ChallengeType } from '../../types'
-import { Avatar, Button } from '../System'
+import { formatTimeLimit } from '../../utils/helperFunctions'
+import { Avatar, Icon } from '../System'
 
 type Props = {
   challengeData: ChallengeType
@@ -17,9 +24,16 @@ const ChallengeStart: FC<Props> = ({ challengeData, handleStartChallenge, setVie
   const user = useSelector(selectUser)
   const router = useRouter()
 
+  const CAN_MOVE = challengeData.gameSettings.canMove
+  const CAN_PAN = challengeData.gameSettings.canMove
+  const CAN_ZOOM = challengeData.gameSettings.canZoom
+  const HAS_TIME_LIMIT = challengeData.gameSettings.timeLimit !== 61
+  const TIME_LIMIT = challengeData.gameSettings.timeLimit
+
   useEffect(() => {
     if (!user.id) {
       setIsLoggedIn(false)
+      console.log(challengeData)
     }
   }, [])
 
@@ -34,19 +48,51 @@ const ChallengeStart: FC<Props> = ({ challengeData, handleStartChallenge, setVie
 
   return (
     <StyledChallengeStart>
-      <h1>You have been challenged!</h1>
-      <div>
-        <Avatar
-          url={`/images/avatars/${challengeData.creatorAvatar}.jpg`}
-          size={40}
-          alt="Challenge Creator Avatar"
-        />
-        <span>{`${challengeData.creatorName} has challenged you to play GeoHub`}</span>
+      <div className="challengeStartWrapper">
+        <h1 className="challengeTitle">You have been challenged!</h1>
+        <div className="challengeCreator">
+          <Avatar
+            url={`/images/avatars/${challengeData.creatorAvatar}.jpg`}
+            size={40}
+            alt="Challenge Creator Avatar"
+          />
+          <span>{`${challengeData.creatorName} has challenged you to play GeoHub`}</span>
+        </div>
+
+        <button className="challengeBtn" onClick={() => handleButtonClick()}>
+          {isLoggedIn ? 'Play Game' : 'Create Account'}
+        </button>
       </div>
 
-      <Button type="solidPurple" callback={() => handleButtonClick()}>
-        {isLoggedIn ? 'Play Game' : 'Create Account'}
-      </Button>
+      <div className="challengeSettings">
+        <div className="settingsItem">
+          <Icon size={30} fill={HAS_TIME_LIMIT ? 'var(--green-400)' : '#888'}>
+            <ClockIcon />
+          </Icon>
+          {HAS_TIME_LIMIT ? `${formatTimeLimit(TIME_LIMIT)} per round` : 'No Time Limit'}
+        </div>
+
+        <div className="settingsItem">
+          <Icon size={30} fill={CAN_MOVE ? 'var(--green-400)' : '#888'}>
+            <ArrowsExpandIcon />
+          </Icon>
+          {CAN_MOVE ? 'Moving Allowed' : 'No Move'}
+        </div>
+
+        <div className="settingsItem">
+          <Icon size={30} fill={CAN_MOVE ? 'var(--green-400)' : '#888'}>
+            <SwitchHorizontalIcon />
+          </Icon>
+          {CAN_PAN ? 'Panning Allowed' : 'No Pan'}
+        </div>
+
+        <div className="settingsItem">
+          <Icon size={30} fill={CAN_MOVE ? 'var(--green-400)' : '#888'}>
+            <ZoomInIcon />
+          </Icon>
+          {CAN_ZOOM ? 'Zooming Allowed' : 'No Zoom'}
+        </div>
+      </div>
     </StyledChallengeStart>
   )
 }
