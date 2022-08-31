518bafff973616e894dfca74e338da50de4583b1
Ben <benhoeg@outlook.com>
2022-08-31 16:33:15 -0300

    Fix: stupid bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
-      },
-    ]
-  },
 }
