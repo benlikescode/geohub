4807b7df4e1f55bb7bd78340eeee159ea5c78dcb
Ben <benhoeg@outlook.com>
2022-09-03 04:12:49 -0300

    fix: typo in new absolute import

--- backend/middlewares/mongoConnection.ts
+++ backend/middlewares/mongoConnection.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { dbConnect } from '@utils/dbConnect'
+import { dbConnect } from '@backend/utils/dbConnect'
 
 // NOT USING THIS
 export const connectToAppDbMiddleware = async (req: NextApiRequest, res: NextApiResponse, next: any) => {
