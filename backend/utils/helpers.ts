0e966c1833c994d9952999b095ee0b535cdfbdf5
Ben <benhoeg@outlook.com>
2022-09-23 03:00:11 -0300

    fix: another typo lol

--- backend/utils/helpers.ts
+++ backend/utils/helpers.ts
@@ -39,7 +39,7 @@ export const getLocations = async (mapId: string, count: number = 1) => {
   // Otherwise: get random locations from DB
 
   const locations = await collections.locations
-    ?.aggregate([{ $match: { mapId: new ObjectId(mapId) }, $sample: { size: count } }])
+    ?.aggregate([{ $match: { mapId: new ObjectId(mapId) } }, { $sample: { size: count } }])
     .toArray()
 
   if (!locations || locations.length === 0) return null
