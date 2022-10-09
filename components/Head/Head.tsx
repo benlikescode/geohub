1af0e613acabe2934543aff5f9104a01a26e15ad
Ben <benhoeg@outlook.com>
2022-10-09 01:39:02 -0300

    chore: added theme-color meta tag

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -20,6 +20,7 @@ const Head: FC<Props> = ({ title, description, ogUrl, ogImage }) => {
       <title>{title || defaultTitle}</title>
       <meta name="description" content={description || defaultDescription} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="var(--background1)" />
 
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
