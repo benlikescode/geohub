c7f3fd4ad999d8f1d7049af531e988e857b2b317
Ben <benhoeg@outlook.com>
2023-05-15 14:52:35 -0300

    fix: deploy

--- backend/routes/maps/getOfficialMaps.ts
+++ backend/routes/maps/getOfficialMaps.ts
@@ -1,3 +1,6 @@
+import { NextApiResponse } from 'next'
+import NextApiRequestWithSession from '../../types/NextApiRequestWithSession'
+
 const getOfficialMaps = async (req: NextApiRequestWithSession, res: NextApiResponse) => {}
 
 export default getOfficialMaps
--- backend/routes/maps/getPopularCustomMaps.ts
+++ backend/routes/maps/getPopularCustomMaps.ts
@@ -1,3 +1,6 @@
+import { NextApiResponse } from 'next'
+import NextApiRequestWithSession from '../../types/NextApiRequestWithSession'
+
 const getPopularCustomMaps = async (req: NextApiRequestWithSession, res: NextApiResponse) => {}
 
 export default getPopularCustomMaps
