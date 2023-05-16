7ff58a25c51853f546722d2b885910b85d7b6c56
Ben <benhoeg@outlook.com>
2023-05-16 20:18:32 -0300

    chore: NotFound design improvements

--- components/ErrorViews/NotFound/NotFound.Styled.tsx
+++ components/ErrorViews/NotFound/NotFound.Styled.tsx
@@ -18,36 +18,23 @@ const StyledNotFound = styled.div<StyledProps>`
     user-select: none;
 
     img {
-      filter: grayscale(1) drop-shadow(-5px 5px 5px #000000);
-      height: 250px;
-      width: 250px;
+      filter: grayscale(0.85);
     }
 
     h2 {
-      font-size: 1.75rem;
-      margin-top: 1.5rem;
-      color: #fff;
+      font-size: 20px;
+      color: #dadada;
       font-weight: 500;
       line-height: 35px;
       text-align: center;
-
-      @media (max-width: 600px) {
-        font-size: 1.25rem;
-      }
+      margin-top: 8px;
     }
 
     h3 {
-      margin-top: 1.25rem;
-      color: #ababab;
-      font-size: 1.25rem;
+      margin-top: 10px;
+      color: #6a6a6a;
+      font-size: 16px;
       font-weight: 400;
-      text-align: center;
-      line-height: 30px;
-
-      @media (max-width: 600px) {
-        font-size: 1rem;
-        line-height: 26px;
-      }
     }
   }
 `
