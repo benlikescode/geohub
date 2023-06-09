f7518d4b990f2972eedacd01ff56dc765054d0f3
Ben <benhoeg@outlook.com>
2023-06-09 14:15:50 -0300

    chore: deleted unused files in backend

--- backend/types/NextApiRequestWithSession.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest } from 'next'
-import { Session } from 'next-auth'
-
-// TO DELETE
-interface NextApiRequestWithSession extends NextApiRequest {
-  userId: Session['user']['id'] | undefined
-}
-
-export default NextApiRequestWithSession
