002cd0913f6df0845e10873174377baa5a6b22b3
Ben <benhoeg@outlook.com>
2023-06-12 21:28:43 -0300

    remove daily challenge map id (since now deleted from DB)

--- utils/constants/random.tsx
+++ utils/constants/random.tsx
@@ -1,7 +1,6 @@
 export const URBAN_WORLD_ID = '631d1a5be3615f68c5ffc4eb'
 export const OFFICIAL_WORLD_ID = '6185df7a7b54baf63473a53e'
 export const COUNTRY_STREAKS_ID = 'country-streaks'
-export const DAILY_CHALLENGE_ID = '63349eb5090804522c2180b7'
 
 export const COUNTRY_STREAK_DETAILS = {
   _id: COUNTRY_STREAKS_ID,
@@ -12,7 +11,6 @@ export const COUNTRY_STREAK_DETAILS = {
 }
 
 export const DAILY_CHALLENGE_DETAILS = {
-  _id: DAILY_CHALLENGE_ID,
   name: 'The Daily Challenge',
   description: 'A brand new challenge everyday. See how you compare!',
   previewImg: 'official8.jpg',
