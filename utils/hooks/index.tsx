1753a9b0c0ea09822f0123c2c80137acd8f745aa
Ben <benhoeg@outlook.com>
2023-06-12 21:29:52 -0300

    created confirm leave hook for map creator page

--- utils/hooks/index.tsx
+++ utils/hooks/index.tsx
@@ -1,3 +1,4 @@
 export { default as useClickOutside } from './useClickOutside'
+export { default as useConfirmLeave } from './useConfirmLeave'
 export { default as useIsMobile } from './useIsMobile'
 export { default as useViewport } from './useViewport'
