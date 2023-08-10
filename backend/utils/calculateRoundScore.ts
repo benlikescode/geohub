74afa4a943bb6c2b0e405a3f42bdfa605e3e4572
Ben Hoeg <63207900+benlikescode@users.noreply.github.com>
2023-08-10 15:14:09 -0300

    give perfect score if within 25m (#47)

--- backend/utils/calculateRoundScore.ts
+++ backend/utils/calculateRoundScore.ts
@@ -1,5 +1,9 @@
 // Calculates the points based on distance away (assumes distance is in km)
 const calculateRoundScore = (distance: number, scoreFactor?: number) => {
+  if (distance * 1000 < 25) {
+    return 5000
+  }
+
   const mapFactor = scoreFactor || 2000
 
   const power = (distance * -1) / mapFactor
