d0eed38d3d9b6591fab28ee8d308e1f40ce660b4
Ben <benhoeg@outlook.com>
2023-08-29 02:54:35 -0300

    allow userId param to get other users published maps

--- backend/routes/maps/getCustomMaps.ts
+++ backend/routes/maps/getCustomMaps.ts
@@ -5,6 +5,21 @@ import { collections, getUserId, throwError } from '@backend/utils'
 // HALP -> likely want to paginate in future
 const getCustomMaps = async (req: NextApiRequest, res: NextApiResponse) => {
   const userId = await getUserId(req, res)
+  const queryUserId = req.query.userId as string
+
+  if (queryUserId) {
+    const customMaps = await collections.maps
+      ?.find({ creator: new ObjectId(userId), isPublished: true, isDeleted: { $exists: false } })
+      .sort({ createdAt: -1 })
+      .limit(9)
+      .toArray()
+
+    if (!customMaps) {
+      return throwError(res, 400, 'Could not retrieve your maps')
+    }
+
+    return res.status(200).send(customMaps)
+  }
 
   const customMaps = await collections.maps
     ?.find({ creator: new ObjectId(userId), isDeleted: { $exists: false } })
