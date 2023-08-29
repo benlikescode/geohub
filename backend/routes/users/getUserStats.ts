670c5758ad83edd727abaa2354c988a93492b0b6
Ben <benhoeg@outlook.com>
2023-08-29 02:53:48 -0300

    create endpoint to get user stats

---
+++ backend/routes/users/getUserStats.ts
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { collections, getUserId, throwError } from '@backend/utils'
+
+const getUserStats = async (req: NextApiRequest, res: NextApiResponse) => {
+  const userId = req.query.userId as string
+
+  const queryFinishedGames = { userId: new ObjectId(userId), state: 'finished' }
+  const queryFinishedStreakGames = { userId: new ObjectId(userId), state: 'finished', mode: 'streak' }
+
+  const gamesPlayed = await collections.games?.find(queryFinishedGames).count()
+  const bestGame = await collections.games?.findOne(queryFinishedGames, { sort: { totalPoints: -1 } })
+  const averageGameScore = await collections.games
+    ?.aggregate([
+      { $match: queryFinishedGames },
+      {
+        $group: {
+          _id: null,
+          avgScore: { $avg: '$totalPoints' },
+        },
+      },
+    ])
+    .toArray()
+
+  const streakGamesPlayed = await collections.games?.find(queryFinishedStreakGames).count()
+  const bestStreakGame = await collections.games?.findOne(queryFinishedStreakGames, { sort: { totalPoints: -1 } })
+
+  const dailyChallengeWins = await collections.challenges
+    ?.find({ isDailyChallenge: true, winner: new ObjectId(userId) })
+    .count()
+
+  const result = {
+    gamesPlayed: gamesPlayed || 0,
+    bestGameScore: bestGame?.totalPoints || 0,
+    averageGameScore: averageGameScore ? Math.ceil(averageGameScore[0]?.avgScore) : 0,
+    streakGamesPlayed: streakGamesPlayed || 0,
+    bestStreakGame: bestStreakGame?.streak || 0,
+    dailyChallengeWins: dailyChallengeWins || 0,
+  }
+
+  res.status(200).send(result)
+}
+
+export default getUserStats
