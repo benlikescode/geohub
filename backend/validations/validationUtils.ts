f1dc1c29277e0c41ce4d62e711e6680288ec6373
Ben <benhoeg@outlook.com>
2024-03-21 17:05:24 -0300

    fix: typo

--- backend/validations/validationUtils.ts
+++ backend/validations/validationUtils.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const stringWithMinMax = (field: string, min: number, max: number) => {
   const minMsg = min > 1 ? `${field} must be at least ${min} characters` : `${field} can not be empty`
-  const maxMsg = `Name must be at most ${max} characters`
+  const maxMsg = `${field} must be at most ${max} characters`
 
   return z
     .string({
