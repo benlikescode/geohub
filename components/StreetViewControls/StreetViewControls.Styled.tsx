b896ea2aa8a0a8d18bc6b02b731fc1c2d79d8028
Eir Wilson <git@eirrw.com>
2024-03-30 18:36:17 -0400

    Fix styling in styled file

--- components/StreetViewControls/StreetViewControls.Styled.tsx
+++ components/StreetViewControls/StreetViewControls.Styled.tsx
@@ -9,7 +9,8 @@ const StyledStreetViewControls = styled.div`
   gap: 15px;
 
   .control-button-wrapper {
-      position: relative;
+    position: relative;
+
     .control-button {
       display: flex;
       align-items: center;
